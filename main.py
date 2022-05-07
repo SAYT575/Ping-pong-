452f819ece4f5396c997d61ed70b9027e1181810
Maks <61080202+SAYT575@users.noreply.github.com>
2022-05-07 17:08:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,49 @@ class Player(GameSprite):
       
      win_width = 600
      win_height = 500
+
+      
+back = (200,255,255)
+win_width, win_height = 600, 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+raket1 = Player("racket.png", 30, 200, 4, 50)
+raket2 = Player("racket.png", 30, 200, 4, 50)
+
+ball = GameSprite("ball.png", 200,200,4,50,50)
+font.init()
+font = font.Font(None,35)
+lose1 = font.render("Player 1 lose!", True, (180,0,0))
+lose2 = font.render("Player 2 lose!", True, (180,0,0))
+speed_x = 3
+speed_y = 3
+
+while game:
+  for e in event.get():
+    if e.type == QUIT:
+  if finish != True:
+      window.fill(back)
+      racket1.update_l()
+      recket2.update_r()
+      ball.rect.x += speed_x
+      ball.rect.y += speed_y
+      if sprite.collide_rect(racket1, ball) or sprite.collide_rect(raket, ball):
+        speed_x *= -1
+        speed_y *= -1
+      if ball.rect.x > win_height - 50 or ball.rect.y:
+        speed_y *= -1 
+      if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        game = False 
+      if ball.rect.x > win_width - 50 or ball.rect.x:
+        finish = True 
+        window.blit(lose2, (200,200))
+      racket1.reset()
+      raket2.reset()
+      ball.reset()
+      
+    display.update()
+    clock.tick(FPS)
+      
+        
